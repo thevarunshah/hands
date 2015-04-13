7b3e5fc9bf8d90ab8fb45afeb82c522fb7ac64a5
Varun Shah <thevarunshah@gmail.com>
2015-04-12 23:55:38 -0400

    started moving stuff to backend
    cleaning up code and separating it into different methods in the backend
    class so it is easier to understand and maintain

--- src/backend/HandsBackend.java
+++ src/backend/HandsBackend.java
@@ -1,5 +1,161 @@
 package backend;
 
+import java.util.ArrayList;
+
+import classes.Card;
+import classes.Deck;
+import classes.Player;
+
 public class HandsBackend {
+	
+	private static ArrayList<Player> players = new ArrayList<Player>(5);
+	private static Deck deck = new Deck();
+	
+	public static void createPlayers(){
+		
+		System.out.println("How many players will there be? (2-5)");
+		int numPlayers = IO.readInt();
+		while(numPlayers < 2 || numPlayers > 5){
+			System.out.println("You must have between 2 and 5 players - please reenter the number of players: ");
+			numPlayers = IO.readInt();
+		}
+		
+		//get player names
+		for(int i = 0; i < numPlayers; i++){
+			int playerNum = i+1;
+			System.out.println("Enter the name of player " + playerNum + ": ");
+			String name = IO.readString();
+			Player player = new Player(name);
+			players.add(player);
+		}
+	}
+	
+	public static void newRound(){
+		
+		for(Player p : players){
+			p.getHand().clear();
+			p.setHandsEstimation(0);
+			p.setCurrentNumHands(0);
+		}
+	}
+	
+	//returns the index of the trump card
+	public static int distributeCards(int roundNum, Card[] deckCards){
+		
+		int deckIndex = 0;
+		for(int i = 0; i < 11-roundNum; i++){
+			for(Player p : players){
+				p.getHand().add(deckCards[deckIndex]);
+				deckIndex++;
+			}
+		}
+		
+		return deckIndex;
+	}
+	
+	//returns the index of the dealer in the players list
+	public static int setDealer(int roundNum){
+		
+		int dealerIndex = 0;
+		for(int i = 0; i < players.size(); i++){
+			Player p = players.get(i);
+			if(roundNum == 1){
+				players.get(players.size()-1).setDealer(true);
+				dealerIndex = players.size()-1;
+				return dealerIndex;
+			}
+			else{
+				if(p.isDealer()){
+					p.setDealer(false);
+					if(i+1 == players.size()){
+						players.get(0).setDealer(true);
+						dealerIndex = 0;
+					}
+					else{
+						players.get(i+1).setDealer(true);
+						dealerIndex = i+1;
+					}
+					return dealerIndex;
+				}
+			}
+		}
+		
+		return dealerIndex;
+	}
+	
+	public static int getStartIndex(int roundNum, int dealerIndex){
+		
+		int startIndex = 0;
+		if(roundNum != 1){
+			if(dealerIndex+1 != players.size()){
+				startIndex = dealerIndex+1;
+			}
+		}
+		
+		return startIndex;
+	}
+	
+	private static void printPlayerHand(Player p){
+		
+		ArrayList<Card> pHand = p.getHand();
+		
+		System.out.println("Your hand: ");
+		for(int j = 0; j < pHand.size(); j++){
+			if(j+1 != pHand.size()){
+				System.out.print(pHand.get(j) + " | ");
+			}
+			else{
+				System.out.println(pHand.get(j));
+			}
+		}
+	}
+	
+	public static void setHandEstimations(int roundNum, int dealerIndex, int startIndex){
+		
+		//ask everyone for hand estimates except dealer
+		for(int i = startIndex; i != dealerIndex; i++){
+			
+			Player p = players.get(i);
+			System.out.println("Player: " + p.getName());
+			printPlayerHand(p);
+			
+			System.out.println("How many hands do you think you will be able to make?");
+			p.setHandsEstimation(IO.readInt());
+			System.out.println();
+			
+			if(i+1 == players.size()){
+				i = -1;
+			}
+		}
+		
+		//dealer declares hands
+		Player dealer = HandsBackend.getPlayers().get(dealerIndex);
+		System.out.println("Player: " + dealer.getName());
+		printPlayerHand(dealer);
+		
+		//num hands already declared
+		int totalHands = 0;
+		for(Player p : players){
+			totalHands += p.getHandsEstimation();
+		}
+		
+		System.out.println("You are the dealer, the total number of hands delared so far is: " + totalHands);
+		System.out.println("How many hands do you think you will be able to make?");
+		int dealerHands = IO.readInt();
+		while(dealerHands+totalHands == 11-roundNum){
+			System.out.println("The total number of hands cannot equal the cards in this round, please reenter");
+			dealerHands = IO.readInt();
+		}
+		dealer.setHandsEstimation(dealerHands);
+		System.out.println();
+	}
+	
+	public static ArrayList<Player> getPlayers() {
+		return players;
+	}
+	
+	public static Deck getDeck() {
+		return deck;
+	}
 
 }
--- src/backend/HandsGameplay.java
+++ src/backend/HandsGameplay.java
@@ -3,148 +3,61 @@ package backend;
 import java.util.ArrayList;
 
 import classes.Card;
-import classes.Deck;
 import classes.Player;
 import classes.Suit;
 
 public class HandsGameplay {
-	
-	private static ArrayList<Player> players = new ArrayList<Player>(5);
-	private static Deck deck = new Deck();
+
+	private static Card[] deckCards;
 
 	public static void main(String[] args) {
-		
-		Card[] deckCards = deck.getCards();
-		
-		Player alice = new Player("Alice");
-		Player bob = new Player("Bob");
-		Player evil = new Player("Evil");
-		
-		players.add(alice);
-		players.add(bob);
-		players.add(evil);
-		
+
+		System.out.println("Welcome to Hands!");
+		deckCards = HandsBackend.getDeck().getCards();
+
+		HandsBackend.createPlayers();
+
 		int roundNum = 1;
-		
+		//start game
 		while(true){
-			
-			deck.shuffleDeck();
-			
+
 			System.out.println("Round " + roundNum);
-			
+			HandsBackend.getDeck().shuffleDeck();
+
 			//fresh start
-			for(Player p : players){
-				p.getHand().clear();
-				p.setHandsEstimation(0);
-				p.setCurrentNumHands(0);
-			}
-			
-			//distribute cards
-			int deckIndex = 0;
-			for(int i = 0; i < 11-roundNum; i++){
-				for(Player p : players){
-					p.getHand().add(deckCards[deckIndex]);
-					deckIndex++;
-				}
-			}
-			
-			Suit trumpSuit = deckCards[deckIndex].getSuit();
+			HandsBackend.newRound();
+
+			//distribute cards and get trump card index
+			int trumpIndex = HandsBackend.distributeCards(roundNum, deckCards);
+
+			Suit trumpSuit = deckCards[trumpIndex].getSuit();
 			System.out.println("The trump suit for this round is: " + trumpSuit + "\n");
-			
-			//set dealer
-			int dealerIndex = 0;
-			for(int i = 0; i < players.size(); i++){
-				Player p = players.get(i);
-				if(roundNum == 1){
-					players.get(players.size()-1).setDealer(true);
-					dealerIndex = players.size()-1;
-					break;
-				}
-				else{
-					if(p.isDealer()){
-						p.setDealer(false);
-						if(i+1 == players.size()){
-							players.get(0).setDealer(true);
-							dealerIndex = 0;
-						}
-						else{
-							players.get(i+1).setDealer(true);
-							dealerIndex = i+1;
-						}
-						break;
-					}
-				}
-			}
-			
-			//start index
-			int startIndex = 0;
-			if(roundNum != 1){
-				if(dealerIndex+1 != players.size()){
-					startIndex = dealerIndex+1;
-				}
-			}
-			
+
+			//set dealer and get the index of the dealer
+			int dealerIndex = HandsBackend.setDealer(roundNum);
+			int startIndex = HandsBackend.getStartIndex(roundNum, dealerIndex);
+
 			//get hand estimations
-			for(int i = startIndex; i != dealerIndex; i++){
-				
-				Player p = players.get(i);
-				System.out.println("Player: " + p.getName());
-				ArrayList<Card> pHand = p.getHand();
-				
-				System.out.println("Your hand: ");
-				for(int j = 0; j < pHand.size(); j++){
-					System.out.print(pHand.get(j) + ";\t");
-				}
-				System.out.println("\n");
-				
-				System.out.println("How many hands do you think you will be able to make?");
-				p.setHandsEstimation(IO.readInt());
-				
-				if(i+1 == players.size()){
-					i = -1;
-				}
-			}
-			//dealer declares hands
-			Player dealer = players.get(dealerIndex);
-			System.out.println("Player: " + dealer.getName());
-			ArrayList<Card> pHand = dealer.getHand();
-			
-			System.out.println("Your hand: ");
-			for(int j = 0; j < pHand.size(); j++){
-				System.out.print(pHand.get(j) + ";\t");
-			}
-			System.out.println("\n");
-			
-			int totalHands = 0;
-			for(Player tmp : players){
-				totalHands += tmp.getHandsEstimation();
-			}
-			System.out.println("You are the dealer, the total number of hands delared so far is: " + totalHands);
-			System.out.println("How many hands do you think you will be able to make?");
-			int dealerHands = IO.readInt();
-			while(dealerHands+totalHands == 11-roundNum){
-				System.out.println("The total number of hands cannot equal the cards in this round, please re-enter");
-				dealerHands = IO.readInt();
-			}
-			dealer.setHandsEstimation(dealerHands);
-			
+			HandsBackend.setHandEstimations(roundNum, dealerIndex, startIndex);
+
 			//play this round
 			for(int i = 0; i < 11-roundNum; i++){
+				
 				ArrayList<Card> pile = new ArrayList<Card>();
 				Suit pileSuit = null;
 				//everyone plays turn
 				for(int j = startIndex; j != dealerIndex; j++){
-					
-					Player p = players.get(j);
+
+					Player p = HandsBackend.getPlayers().get(j);
 					System.out.println("Player: " + p.getName());
 					ArrayList<Card> playerHand = p.getHand();
-					
+
 					System.out.println("Your hand: ");
 					for(int k = 0; k < playerHand.size(); k++){
 						System.out.print(k+1 + ") " + playerHand.get(k) + "  ");
 					}
 					System.out.println("\n");
-					
+
 					System.out.println("Which card would you like to play? (enter number)");
 					if(j == startIndex){
 						int cardIndex = IO.readInt();
@@ -186,22 +99,22 @@ public class HandsGameplay {
 						pile.add(p.getHand().get(cardIndex-1));
 						p.getHand().remove(cardIndex-1);
 					}
-					
-					if(j+1 == players.size()){
+
+					if(j+1 == HandsBackend.getPlayers().size()){
 						j = -1;
 					}
 				}
 				//dealer plays turn
-				Player p = players.get(dealerIndex);
-				System.out.println("Player: " + p.getName());
-				ArrayList<Card> playerHand = p.getHand();
-				
+				Player dealer = HandsBackend.getPlayers().get(dealerIndex);
+				System.out.println("Player: " + dealer.getName());
+				ArrayList<Card> playerHand = dealer.getHand();
+
 				System.out.println("Your hand: ");
 				for(int j = 0; j < playerHand.size(); j++){
 					System.out.print(j+1 + ") " + playerHand.get(j) + "  ");
 				}
 				System.out.println("\n");
-				
+
 				System.out.println("Which card would you like to play? (enter number)");
 				System.out.println("Current pile:");
 				for(Card c : pile){
@@ -209,29 +122,29 @@ public class HandsGameplay {
 				}
 				System.out.println();
 				int cardIndex = IO.readInt();
-				while(cardIndex > p.getHand().size()){
+				while(cardIndex > dealer.getHand().size()){
 					System.out.println("Please reenter a valid value");
 					cardIndex = IO.readInt();
 				}
 				//legal card play check
 				boolean hasPileSuit = false;
-				for(Card c : p.getHand()){
+				for(Card c : dealer.getHand()){
 					if(c.getSuit() == pileSuit){
 						hasPileSuit = true;
 						break;
 					}
 				}
-				while(p.getHand().get(cardIndex-1).getSuit() != pileSuit && hasPileSuit){
+				while(dealer.getHand().get(cardIndex-1).getSuit() != pileSuit && hasPileSuit){
 					System.out.println("You must play the suit that is the current pile's suit.");
 					cardIndex = IO.readInt();
-					while(cardIndex > p.getHand().size()){
+					while(cardIndex > dealer.getHand().size()){
 						System.out.println("Please reenter a valid value");
 						cardIndex = IO.readInt();
 					}
 				}
-				pile.add(p.getHand().get(cardIndex-1));
-				p.getHand().remove(cardIndex-1);
-				
+				pile.add(dealer.getHand().get(cardIndex-1));
+				dealer.getHand().remove(cardIndex-1);
+
 				//calculate turn winner
 				Card currentHighest = pile.get(0);
 				for(Card c : pile){
@@ -256,34 +169,34 @@ public class HandsGameplay {
 				int turnWinnerIndex = pile.indexOf(currentHighest);
 				int playerIndex = startIndex;
 				for(int count = 0; count < turnWinnerIndex; count++){
-					if(playerIndex+1 == players.size()){
+					if(playerIndex+1 == HandsBackend.getPlayers().size()){
 						playerIndex = 0;
 						continue;
 					}
 					playerIndex++;
 				}
-				System.out.println(players.get(playerIndex).getName() + ", you won this turn!");
-				players.get(playerIndex).setCurrentNumHands(players.get(playerIndex).getCurrentNumHands()+1);
+				System.out.println(HandsBackend.getPlayers().get(playerIndex).getName() + ", you won this turn!");
+				HandsBackend.getPlayers().get(playerIndex).setCurrentNumHands(HandsBackend.getPlayers().get(playerIndex).getCurrentNumHands()+1);
 			}
-			
-			for(Player p : players){
+
+			for(Player p : HandsBackend.getPlayers()){
 				System.out.println(p.getName() + ": " + p.getCurrentNumHands() + " hands");
 			}
-			
+
 			//calculate round winners
-			for(Player p : players){
+			for(Player p : HandsBackend.getPlayers()){
 				if(p.getCurrentNumHands() == p.getHandsEstimation()){
 					p.setPoints(p.getPoints()+10+p.getCurrentNumHands());
 				}
 			}
-			
-			for(Player p : players){
+
+			for(Player p : HandsBackend.getPlayers()){
 				System.out.println(p.getName() + ": " + p.getPoints() + " points");
 			}
-			
+
 			//win condition
 			boolean exit = false;
-			for(Player p : players){
+			for(Player p : HandsBackend.getPlayers()){
 				if(p.getPoints() >= 75){
 					System.out.println(p.getName() + ", you won!!");
 					exit = true;
@@ -292,7 +205,7 @@ public class HandsGameplay {
 			if(exit){
 				break;
 			}
-			
+
 			roundNum++;
 			if(roundNum == 11){
 				break;
